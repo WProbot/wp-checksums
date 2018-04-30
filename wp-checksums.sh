7c8aae55b73ec5e6d3a7a100c2359cdaec07b26f
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-30 17:26:29 +0100

    Added test for root

--- wp-checksums.sh
+++ wp-checksums.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Test to see if the user is root (we don't want them to be root)
+if [`whoami` == root ]
+then
+	echo && echo "DO NOT run this script as root. Please run this script as a normal user".
+	exit 1
+fi
+
 # Downloads WP-CLI - http://wp-cli.org/
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &>/dev/null
 
