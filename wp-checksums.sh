05cbef9be40408262aa886b87a7c487394e35271
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-30 07:06:56 +0100

    Added comments

--- wp-checksums.sh
+++ wp-checksums.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+# Downloads WP-CLI - http://wp-cli.org/
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
+# Checks to see if it is present and shows its version
 php wp-cli.phar --info
 
+# Checks the checksums of your WordPress website against what they should be
 php wp-cli.phar core verify-checksums
 
+# Removes WP-CLI from the system after work is done
 rm -f wp-cli.phar
 
 exit 0
